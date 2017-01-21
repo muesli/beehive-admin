7141afbcaac8aa1780c65aae4fc53c55087c8380
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-21 01:26:36 +0100

    bee's namespace references a hive-ID

--- app/models/bee.js
+++ app/models/bee.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    namespace: DS.attr('string'),
+    namespace: DS.belongsTo('hive'),
     description: DS.attr('string'),
     lastevent: DS.attr('isodate'),
     lastaction: DS.attr('isodate'),

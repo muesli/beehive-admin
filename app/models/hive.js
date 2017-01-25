fe8c8c32e94d19cd1ad6d21fbecd07128af3634d
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-25 19:30:13 +0100

    Added logocolor to hives model

--- app/models/hive.js
+++ app/models/hive.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string'),
     image: DS.attr('string'),
+    logocolor: DS.attr('string'),
     description: DS.attr('string'),
     isCompleted: DS.attr('boolean'),
     options: DS.attr(),

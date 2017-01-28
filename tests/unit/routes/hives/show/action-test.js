5f3d148d510050cbd583c2f2c7a12c4d2506f724
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-28 06:07:20 +0100

    Added unit tests for hives/show route

---
+++ tests/unit/routes/hives/show/action-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:hives/show/actions/action', 'Unit | Route | hives/show/actions/action', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});

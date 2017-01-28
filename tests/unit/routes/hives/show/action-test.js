06ef21792f7eac2c7992074720b5501fd05d9e61
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-28 06:16:54 +0100

    Fixed unit test for hives/show/action

--- tests/unit/routes/hives/show/action-test.js
+++ tests/unit/routes/hives/show/action-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:hives/show/actions/action', 'Unit | Route | hives/show/actions/action', {
+moduleFor('route:hives/show/action', 'Unit | Route | hives/show/action', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });

a1510944b8bc4adf69f4fcffd3a9e219bcc50f0c
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-29 12:21:32 +0100

    Added bees/all router, template & controller

---
+++ tests/unit/routes/bees/all-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:bees/all', 'Unit | Route | bees/all', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});

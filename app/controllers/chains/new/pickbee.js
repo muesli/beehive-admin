a8210f1d36f06cdcba91c49750f1edfa841e79e7
Christian Muehlhaeuser <muesli@gmail.com>
2017-02-16 05:52:00 +0100

    Show an error if there are no Bee's with events or actions, when we need one

--- app/controllers/chains/new/pickbee.js
+++ app/controllers/chains/new/pickbee.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	event_bees: [],
+	eventBees: Ember.computed('bees.@each', 'event_bees', function() {
+		var b = this.get('event_bees');
+		this.get('bees').forEach(bee => {
+			bee.get('namespace').then(namespace => {
+				if (namespace.get('events').length > 0) {
+					b.pushObject(bee);
+				}
+			});
+		});
+		return b;
+	}),
+
 	actions: {
 		nextStep(bee) {
 			this.transitionToRoute('chains.new.pickevent', bee.id);
--- app/controllers/chains/new/pickbeeaction.js
+++ app/controllers/chains/new/pickbeeaction.js
@@ -4,6 +4,19 @@ export default Ember.Controller.extend({
 	queryParams: ['filters'],
 	filters: [],
 
+	action_bees: [],
+	actionBees: Ember.computed('bees.@each', 'action_bees', function() {
+		var b = this.get('action_bees');
+		this.get('bees').forEach(bee => {
+			bee.get('namespace').then(namespace => {
+				if (namespace.get('actions').length > 0) {
+					b.pushObject(bee);
+				}
+			});
+		});
+		return b;
+	}),
+
 	actions: {
 		nextStep(bee) {
 			this.transitionToRoute('chains.new.pickaction', this.beeevent, this.event, bee.id, {
